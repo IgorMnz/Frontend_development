a905337a3d111ff2c15950925430967cf6a3365f
IgorMnzWork <ib.monzin@gmail.com>
2022-02-02 17:03:10 +0300

    React_employers_start

--- 10_REACT/my-app/src/index.js
+++ 10_REACT/my-app/src/index.js
@@ -1,30 +1,11 @@
-import React, {StrictMode} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import {Header} from './App'
-
-// const elem = <h2 class='greetings'>Hello world!</h2> //Это называется React элемент
-
-// const text = 'Hello World!'
-
-// const elem = (
-//   <div>
-//       <h2 className='greetings'>Текст: {text}</h2>
-//       <input type="text" />
-//       <button tabIndex="0">Click</button>
-//   </div>
-
-// ); //Если элемент имеет многострочную структуру, мы ее оборачиваем в скобки а так же он обязательно должен иметь один корневой элемент(у нас div)
-
-// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello world!');
+import App from './components/app/app';
 
 ReactDOM.render(
-  // elem,
-  <StrictMode>
-    <App/>
-  </StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
- 
