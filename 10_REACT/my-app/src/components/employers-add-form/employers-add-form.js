d4822456bd8010fb2185375ebd898161a089e36f
IgorMnz <ib.monzin@gmail.com>
2022-02-03 23:01:03 +0300

    React_016

--- 10_REACT/my-app/src/components/employers-add-form/employers-add-form.js
+++ 10_REACT/my-app/src/components/employers-add-form/employers-add-form.js
@@ -1,24 +1,50 @@
+import { Component } from 'react/cjs/react.production.min';
 import './employers-add-form.css'
 
 
-const EmployersAddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Добавьте нового сотрудника</h3>
-            <form
-                className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="Как его зовут?" />
-                <input type="number"
-                    className="form-control new-post-label"
-                    placeholder="З/П в $?" />
+//Мы в стейт помещаем значение value из инпута name и salary. Затем в один метод onValueChange мы прокидываем изменение нашего стейта для name и salary путем добавления атрибутов в саму верстку. Все что мы вводим в инпут контролируется при помощт state который записывается в этот компонент, это дает нам то, что стейт синхронизирован с интерфейсом (UI)
+class EmployersAddForm extends Component {
+    constructor(props) {
+        super(props);        
+        this.state = {
+            name: '',
+            salary: ''
+        }
+    }
 
-                <button type="submit"
-                        className="btn btn-outline-light">Добавить</button>
-            </form>
-        </div>
-    )
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render() {
+        const {name, salary} = this.state;
+
+        return (
+            <div className="app-add-form">
+                <h3>Добавьте нового сотрудника</h3>
+                <form
+                    className="add-form d-flex">
+                    <input type="text"
+                        className="form-control new-post-label"
+                        placeholder="Как его зовут?"
+                        name="name"
+                        value={name} 
+                        onChange={this.onValueChange}/>
+                    <input type="number"
+                        className="form-control new-post-label"
+                        placeholder="З/П в $?"
+                        name="salary"
+                        value={salary} 
+                        onChange={this.onValueChange}/>
+    
+                    <button type="submit"
+                            className="btn btn-outline-light">Добавить</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 
--- 10_REACT/my-app/src/components/employers-list-item/employers-list-item.js
+++ 10_REACT/my-app/src/components/employers-list-item/employers-list-item.js
@@ -1,34 +1,70 @@
+import { Component } from 'react/cjs/react.production.min';
 import './employers-list-item.css'
 
 
 //каждый отдельный сотрудник который у нас будет находится в приложении. defaultValue в React будет показывать значение по умолчанию
-const EmployersListItem = ({name, salary, increase}) => {
+class EmployersListItem extends Component{
+    constructor(props) {
+        super(props);
 
-    let classNames = 'list-group-item d-flex justify-content-between';
-
-    if (increase) {
-        classNames += ' increase'
+        this.state = {
+            increase: false,
+            rise: false
+        }
     }
 
+    onIncrease = () => {
+        this.setState(({increase}) =>({
+            increase: !increase
+        }))
+    }
 
-    return (
-        <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + ' $'}/> 
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
+    onRise = () => {
+        this.setState(({rise}) =>({
+            rise: !rise
+        }))
+    }
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+    //В константах деструктурируем объект чтобы потом в условиях if просто использовать названия пропсов. Мы навешиваем обработчик события onClick на span и с помощью метода onRise мы добавляем класс like, и при этом отслеживаем какое значение state было до этого, поэтому в this.state выше прописываем что rise: false
+    render() {
+        const {name, salary} = this.props;
+        const {increase, rise} = this.state;
+
+        let classNames = 'list-group-item d-flex justify-content-between';
+
+        if (increase) {
+            classNames += ' increase'
+        }
+
+        if(rise) {
+            classNames += ' like'
+        }
+    
+    
+        return (
+            <li className={classNames}>
+                <span className="list-group-item-label" onClick={this.onRise}>{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + ' $'}/> 
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                        className="btn-cookie btn-sm "
+                        onClick={this.onIncrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+    
+                    <button type="button"
+                            className="btn-trash btn-sm ">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
+
 }
 
 export default EmployersListItem;
+
+
+//<span className="list-group-item-label" onClick={this.onLike}>{name}</span>
