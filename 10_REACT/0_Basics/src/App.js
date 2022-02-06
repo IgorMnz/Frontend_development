49daf2c635231b879666024c0e266a3b173a9e98
IgorMnz <ib.monzin@gmail.com>
2022-02-06 13:48:04 +0300

    React_020

--- 10_REACT/0_Basics/src/App.js
+++ 10_REACT/0_Basics/src/App.js
@@ -88,7 +88,7 @@ class WhoAmI extends Component {
 		const {name, surname, link} = this.props;
 		const {position, years} = this.state;
 		return (
-			<div>
+			<>
 				<button onClick={this.nextYear}>{this.state.text}</button>
 				<h1>My name is {name}, 
 					surname - {surname}, 
@@ -100,7 +100,7 @@ class WhoAmI extends Component {
 					<span>Введите должность</span>
 					<input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')}/>
 				</form>
-			</div>
+			</>
 		)
 	}
 }
