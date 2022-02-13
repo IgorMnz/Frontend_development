a6667c3e9f3f5face5eea5c42862f1876085f78b
IgorMnz <ib.monzin@gmail.com>
2022-02-13 20:44:54 +0300

    Marvel_class_done

---
+++ 10_REACT/2_Marvel_class_components/src/services/MarvelService.js
@@ -0,0 +1,44 @@
+//Мы не наследуем от Component так как мы класс на чистом JS
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey ='apikey=be23d48578ce8a8865c373b78faaa289';
+    _baseOffset = 210;
+
+    getResources = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status} `)
+        }
+
+        return await res.json();
+    }
+
+    //Для того чтобы потом можно было вызывать функцию с необходимым нам оффсетом, мы передаем этот оффсет как аргумент и по умолчанию он будет равным тому, который мы установили выше
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResources(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResources(`${this._apiBase}characters/${id}?&${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    //Если у нас нету описания то возвращается текст что нету описания, а так же ограничение в 200 символов
+    _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 200)}...` : 'There is no description for this character...',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+
+            comics: char.comics.items.slice(0, 10)
+        }
+    }
+
+}
+
+export default MarvelService;
