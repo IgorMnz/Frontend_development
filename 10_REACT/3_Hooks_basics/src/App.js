9f61a39ba6d1d1c1bde9f89c0f8752b79b9b2d9e
IgorMnz <ib.monzin@gmail.com>
2022-02-14 21:41:50 +0300

    Converter_add

--- 10_REACT/3_Hooks_basics/src/App.js
+++ 10_REACT/3_Hooks_basics/src/App.js
@@ -1,4 +1,4 @@
-import {Component, useState} from 'react';
+import {Component, useState, useEffect} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
@@ -48,27 +48,71 @@ import './App.css';
 // }
 
 
+// const calcValue = () => {
+//     console.log('random');
+
+//     return Math.random() * (50 - 1) + 1;
+// }
+
 const Slider = (props) => {
 
-    const slideStateArray = useState();
+    const [slide, setSlide] = useState(0);
+    const [autoplay, setAutoplay] = useState(false);
 
-    console.log(slideStateArray)
+    function logging () {
+        console.log('log!')
+    }
+
+    useEffect(() => {
+        console.log('effect')
+        document.title = `Slide: ${slide}`;
+
+        // window.addEventListener('click', logging)
+
+        // return () => {
+        //     window.removeEventListener('click', logging)
+        // }
+
+    }, [slide])
+
+    useEffect(() => {
+        console.log('autoplay')
+    }, [autoplay])
+
+    function changeSlide(i) {
+        setSlide(slide => slide + i);
+    }
+
+    function toggleAutoplay () {
+        setAutoplay(autoplay => !autoplay)
+    }
+
+    //Для того чтобы в одном useState прописать сразу все  стейты: (потом в верстве используем {state.slide} и {state.autoplay})
+    // const [state, setState] = useState({slide: 0, autoplay: false});
+
+    // function changeSlide(i) {
+    //     setState(state => ({...state, slide: state.slide + i}));
+    // }
+
+    // function toggleAutoplay () {
+    //     setState(state => ({...state, autoplay: !state.autoplay}))
+    // }
 
     return (
         <Container>
             <div className="slider w-50 m-auto">
                 <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-                <div className="text-center mt-5">Active slide {this.state.slide} <br/> {this.state.autoplay ? 'auto' : null}</div>
+                <div className="text-center mt-5">Active slide {slide} <br/>{autoplay ? 'auto' : null}</div>
                 <div className="buttons mt-3">
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={() => this.changeSlide(-1)}>-1</button>
+                        onClick={() => changeSlide(-1)}>-1</button>
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={() => this.changeSlide(1)}>+1</button>
+                        onClick={() => changeSlide(1)}>+1</button>
                     <button 
                         className="btn btn-primary me-2"
-                        onClick={this.toggleAutoplay}>toggle autoplay</button>
+                        onClick={toggleAutoplay}>toggle autoplay</button>
                 </div>
             </div>
         </Container>
@@ -77,8 +121,17 @@ const Slider = (props) => {
 
 
 function App() {
+    const [slider, setSlider] = useState(true)
+
+
+
   return (
-        <Slider/>
+        <>
+            <button onClick={() => setSlider(false)}>Click</button>
+            {slider ? <Slider/> : null}
+        </>
+
+
   );
 }
 
