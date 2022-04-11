ce63fc3143456d8fd8aa0a0c87913aa9e29ee2a8
asp-mib <asp-mib@rosenergoatom.ru>
2022-04-11 16:46:17 +0300

    hero_admin_panel_Finish

--- 10_REACT/12_hero_admin_panel/src/store/index.js
+++ 10_REACT/12_hero_admin_panel/src/store/index.js
@@ -1,6 +1,9 @@
-import { createStore } from 'redux';
-import reducer from '../reducers';
+import { createStore, combineReducers } from 'redux';
+import heroes from '../reducers/heroes';
+import filters from '../reducers/filters';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+//С помощью combineReducers совмещаем оба наших reducera, прописывая их в объекте как {heroes: heroes, filters: filters} и прописываем сокращенный вариант:
+const store = createStore(  combineReducers({heroes, filters}),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
