983f27260881f68cd6fd9055607dc49b2957d60d
igor_mnz <ib.monzin@gmail.com>
2022-08-24 11:16:07 +0300

    clone to home directory

--- 3_TypeScript/4_stats/src/util.ts
+++ 3_TypeScript/4_stats/src/util.ts
@@ -5,4 +5,6 @@ export const dateStringtoDate = (dateString: string): Date => {
     }) // [28, 10, 2018]
 
     return new Date(dateParts[2], dateParts[1] - 1, dateParts[0])
+
+    console.log('test')
 }
